354bc51df7d19ec5f830b5d157c2ce381ad9170a
minsukim2827 <m.kim006064@gmail.com>
2024-08-22 15:05:44 +1200

    successfully integrated signin page

--- app/api/database/post/route.ts
+++ app/api/database/post/route.ts
@@ -1,23 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import { faker } from "@faker-js/faker";
 import bcrypt from "bcrypt";
-import {getClient} from "@/scripts/db"
+import {sql} from "@/scripts/db"
 
 export async function POST(request: NextRequest) {
-  const client = await getClient();
 
   try {
-    await client.connect();
-
     const randomPassword = faker.internet.password();
+    console.log("randomPassword: ", randomPassword);
     const hash = await bcrypt.hash(randomPassword, 10);
-
     // Execute the query
-    const { rows } = await client.query(
+    const { rows } = await sql(
       "INSERT INTO public.users (username, password, avatar, is_admin) VALUES ($1, $2, $3, $4) RETURNING *",
       [faker.internet.displayName(), hash, faker.image.avatar(), false]
     );
-
     return NextResponse.json(rows[0], { status: 201 });
   } catch (err) {
     console.error("Database query error", err);
@@ -25,7 +21,5 @@ export async function POST(request: NextRequest) {
       { error: "Internal Server Error" },
       { status: 500 }
     );
-  } finally {
-    await client.end();
-  }
+  } 
 }
