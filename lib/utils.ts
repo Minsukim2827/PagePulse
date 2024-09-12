bcfaec08d595f2dc21df15409fb4791b793e0b02
minsukim2827 <m.kim006064@gmail.com>
2024-09-12 16:33:38 +1200

    utils and book-lists

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-
+import { format } from 'date-fns';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  return format(date, 'dd MM yyyy');
+};
